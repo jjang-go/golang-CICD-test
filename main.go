fecafc71f0e712e6ed6b68a9f45d6a9811a108c4
jjang-go <tykim9532@gmail.com>
2024-03-14 16:44:46 +0900

    update echo

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func echoStart() *echo.Echo {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(200, "success")
+	})
+
+	return e
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	e := echoStart()
+
+	if err := e.Start(":8080"); err != nil {
+		panic(err)
+	}
 }
